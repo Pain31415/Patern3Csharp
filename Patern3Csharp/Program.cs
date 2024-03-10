86d974c339599295e1fd7d4d1b60686e10ad1820
Pain31415 <Cherniy412@ukr.net>
2024-03-10 13:10:10 +0200

    Add project files.

---
+++ Patern3Csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Patern3Csharp.Proxi;
+
+namespace Patern3Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserProxy userProxy = new UserProxy();
+
+            User adminUser = userProxy.GetUser("John", "Admin");
+            if (adminUser != null)
+            {
+                Console.WriteLine("User found: " + adminUser.Name);
+            }
+        }
+    }
+
+}
---
+++ Patern3Csharp/Proxi/UserProxy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patern3Csharp.Proxi
+{
+    public class User
+    {
+        public required string Name { get; set; }
+        public required string Role { get; set; }
+    }
+
+    public class UserFacade
+    {
+        private User[] users;
+
+        public UserFacade()
+        {
+            users = new User[] {
+            new User { Name = "John", Role = "Admin" },
+            new User { Name = "Alice", Role = "User" },
+            new User { Name = "Bob", Role = "User" }
+        };
+        }
+
+        public User? GetUser(string name)
+        {
+            foreach (var user in users)
+            {
+                if (user.Name == name)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class UserProxy : UserFacade
+    {
+        private UserFacade userFacade;
+
+        public UserProxy()
+        {
+            userFacade = new UserFacade();
+        }
+
+        public User? GetUser(string name, string requestingUserRole)
+        {
+            User user = userFacade.GetUser(name);
+
+            if (user != null && user.Role == "Admin")
+            {
+                return user;
+            }
+            else if (user != null && user.Role == "User" && requestingUserRole == "Admin")
+            {
+                return user;
+            }
+            else
+            {
+                Console.WriteLine("Access denied. Insufficient permissions.");
+                return null;
+            }
+        }
+    }
+}
