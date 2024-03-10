4f5288a51ee04d36ec52306a2e484543e4b4f5b4
Pain31415 <Cherniy412@ukr.net>
2024-03-10 13:10:10 +0200

    Add project files.

---
+++ Patern3Csharp/Facade/HomeTheaterFacade.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patern3Csharp.Facade
+{
+    class LightSubsystem
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Lights are on");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Lights are off");
+        }
+    }
+
+    class SoundSubsystem
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Sound system is on");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Sound system is off");
+        }
+    }
+    class HomeTheaterFacade
+    {
+        private LightSubsystem lights;
+        private SoundSubsystem sound;
+
+        public HomeTheaterFacade()
+        {
+            lights = new LightSubsystem();
+            sound = new SoundSubsystem();
+        }
+
+        public void WatchMovie()
+        {
+            Console.WriteLine("Get ready to watch a movie...");
+            lights.TurnOn();
+            sound.TurnOn();
+        }
+
+        public void EndMovie()
+        {
+            Console.WriteLine("Shutting down the movie theater...");
+            lights.TurnOff();
+            sound.TurnOff();
+        }
+    }
+}
---
+++ Patern3Csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Patern3Csharp.Facade;
+
+namespace Patern3Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeTheaterFacade homeTheater = new HomeTheaterFacade();
+
+            homeTheater.WatchMovie();
+
+            homeTheater.EndMovie();
+        }
+    }
+
+}
